2041139462345bb8acc93cc69c9c114f9107de9a
Xiaolou Huang <lor.worwag@gmail.com>
2021-03-06 11:08:56 -0700

    set up ios project

---
+++ ios/food-barcode-scanner/food-barcode-scanner/food_barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_barcode_scannerApp.swift
+//  food-barcode-scanner
+//
+//  Created by Lor Worwag on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct food_barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
