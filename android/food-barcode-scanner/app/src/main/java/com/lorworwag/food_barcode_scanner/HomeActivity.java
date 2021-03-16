9d05abfca48293f986fc305ce3df786238189962
Xiaolou Huang <lor.worwag@gmail.com>
2021-03-16 10:07:01 -0700

    deleted testing files for android

--- android/food-barcode-scanner/app/src/main/java/com/lorworwag/food_barcode_scanner/HomeActivity.java
+++ android/food-barcode-scanner/app/src/main/java/com/lorworwag/food_barcode_scanner/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.lorworwag.food_barcode_scanner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -18,6 +22,7 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private Button btnScanBarcode;
+    private TextView txtBarcode;
     private Button btnSignOut;
 
     @Override
@@ -28,13 +33,13 @@ public class HomeActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        txtBarcode = findViewById(R.id.txtBarcode);
         btnSignOut = findViewById(R.id.btnSignOut);
 
         btnScanBarcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, BarcodeScanActivity.class);
-                startActivity(intent);
+                scanBarcode(v);
             }
         });
 
@@ -49,6 +54,38 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        txtBarcode.setText("abc");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    public void scanBarcode(View view) {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(HomeActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+                txtBarcode.setText("Cancelled");
+            } else {
+                Toast.makeText(HomeActivity.this, intentResult.getContents().toString(), Toast.LENGTH_LONG).show();
+                txtBarcode.setText("abc");
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
